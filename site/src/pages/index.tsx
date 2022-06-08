707d36cf861f865b5bd5acb898d53fb54bb17508
Teffen <TeffenEllis@users.noreply.github.com>
2022-06-08 14:34:24 -0400

    Update description.

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -41,7 +41,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="A batteries-included, magic-free, library for building web apps on Cloudflare Workers.">
       <HomepageHeader />
 
       <div className="background-container" />
