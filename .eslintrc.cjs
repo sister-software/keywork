d35e2011400e95b50750157e66d93ae690ab1c78
Teffen Ellis <teffen@nirri.us>
2023-02-20 16:15:44 -0500

    Use package linters.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,22 +32,7 @@ const config = {
   root: true,
   ignorePatterns: ['./packages/*/dist/**/*', './node_modules/**/*'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc', 'header'],
-  settings: {
-    react: {
-      version: '18.2.0',
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.mts', '.tsx'],
-    },
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['@sister.software/eslint-config'],
   env: {
     browser: true,
     node: true,
@@ -55,29 +40,6 @@ const config = {
   },
   rules: {
     'header/header': [1, 'block', headerLines, 1],
-    'react/prop-types': 'off',
-    'no-undef': 'off',
-    'no-extra-semi': 'off',
-    '@typescript-eslint/no-extra-semi': ['warn'],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        ignoreRestSiblings: true,
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: 'logger',
-      },
-    ],
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/ban-ts-comment': [
-      'warn',
-      {
-        'ts-ignore': 'allow-with-description',
-      },
-    ],
   },
 }
 
